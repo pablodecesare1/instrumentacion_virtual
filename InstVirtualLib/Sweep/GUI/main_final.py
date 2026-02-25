c09bed9d1f01cef99e462e260468ae84725f3276
Jmaurin <jmaurin@frba.utn.edu.ar>
2026-02-24 21:19:43 -0300

    - Eliminamos clases no referidas a instrumentacion

--- InstVirtualLib/Sweep/GUI/main_final.py
+++
@@ -1,9 +0,0 @@
-import pyvisa
-import vxi11
-
-from InstVirtualLib.Sweep.GUI.ui_final import App
-from InstVirtualLib.instrument import Instrument
-from InstVirtualLib.osciloscopios import RIGOL_DS2202
-
-if __name__ == "__main__":
-    App().mainloop()
