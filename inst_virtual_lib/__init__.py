6c059d80422295a466384fb59a4de3c5e0d1b595
Ramiro R.C. <rawthil@gmail.com>
2026-06-26 09:58:25 -0300

    fix init

--- inst_virtual_lib/__init__.py
+++ inst_virtual_lib/__init__.py
@@ -1,3 +1,43 @@
 __author__ = "UTN FRBA R4001"
 __version__ = "0.1.0"
 __credits__ = "UTN-FRBA"
+
+from inst_virtual_lib.analizador_espectro import AnalizadorEspectro, RigolDsa800
+from inst_virtual_lib.generadores_arbitrarios import (
+    Agilent33512A,
+    GeneradorArbitrario,
+    RigolDG5071,
+    Siglent1032X,
+)
+from inst_virtual_lib.instrument import Instrument
+from inst_virtual_lib.mediciones import Mediciones
+from inst_virtual_lib.operador import OperadorGenerador, OperadorOsciloscopio
+from inst_virtual_lib.osciloscopios import (
+    GwInstek,
+    Mso3024A,
+    Osciloscopio,
+    Rigol,
+    RigolDs2202,
+    SDS2102,
+    TektronixDsoDpoMsoTds,
+)
+
+__all__ = [
+    "Agilent33512A",
+    "AnalizadorEspectro",
+    "GeneradorArbitrario",
+    "GwInstek",
+    "Instrument",
+    "Mediciones",
+    "Mso3024A",
+    "OperadorGenerador",
+    "OperadorOsciloscopio",
+    "Osciloscopio",
+    "Rigol",
+    "RigolDG5071",
+    "RigolDsa800",
+    "RigolDs2202",
+    "SDS2102",
+    "Siglent1032X",
+    "TektronixDsoDpoMsoTds",
+]
